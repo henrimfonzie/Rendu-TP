f0b1866a3315eeaf7b3cb8ccd7623e989ee56b44
EnzoPAGANO <ep69devops@gmail.com>
2021-04-27 10:49:16 +0200

    Script fonctionnel, v1.01

--- corbeille/suppr.sh
+++ corbeille/suppr.sh
@@ -1,7 +1,8 @@
+#!/bin/bash
 ###########################
 #	Utilitaire corbeille  #
 #	Tcherno, 26/04/2021   #
-#	Version : alpha		  #
+#	Version : v1.01		  #
 ###########################
 
 #INITIALISATION DE LA CORBEILLE
@@ -22,17 +23,24 @@ rem()
                 ficnom="${1##*/}"       #récupère le nom du fichier (cas où un chemin d'accès est four$
                 info="$ficnom.info"
                 echo $absrep > "$corbeille/.$info"      #crée le fichier .info
+				if [[ $? == 0 ]];then
+					echo -e "Fichier supprime"
+				else
+					echo -e "Erreur lors de la suppression"
+				fi
         elif [[ -d $1 ]];then           #si c'est un dossier
                 absrep=`readlink -f $1`
                 mv $1 $corbeille                #prend en compte le cas d'un dossier non vide
                 ficnom="$1"     #récupère le nom du fichier (cas où un chemin d'accès est fourni)
-                echo $ficnom
                 info="${ficnom/%\//}.info" #épure le chemin d'accès en virant le "/" final
-                echo $info
-                echo $absrep
                 echo $absrep > "$corbeille/.$info"      #crée le fichier .info
+				if [[ $? == 0 ]];then
+					echo -e "Dossier supprime"
+				else
+					echo -e "Erreur lors de la suppression"
+				fi
         else
-                echo -e "Chemin invalide"
+                echo -e "Parametre invalide"
         fi
 
 }
@@ -44,24 +52,45 @@ trash()
 
 #FONCTION DE RESTAURATION
 restore()
-
+{
+	if [[ -e "$corbeille/$1" || -e "$corbeille/$1.info" ]];then  #si le fichier à restaurer existe
+		info="$corbeille/.$1.info"			#on récupère le fichier *.info
+		absrep=$(<$info)					#on récupère son contenu
+		mv "$corbeille/$1" $absrep			#on restaure
+		if [[ $? == 0 ]];then
+			echo -e "Fichier restaure dans $absrep"
+		else
+			echo -e "Erreur lors de la restauration"
+		fi
+	else
+		echo -e "Parametre invalide"
+	fi
+}
 
 #PROGRAMME PRINCIPAL
 #le programme principal vérifie l'entrée utilisateur
 
-if [[ $? == 0 || -z $arg1 || -z $arg2 ]];then
+if [[ $# == 0 || -z $arg1 ]];then
 	echo -e "Parametre invalide" 
 fi	
 	
 case $arg1 in
 	--rm)
-		rem $arg2
+		if [[ -z $arg2 ]];then
+			echo -e "Parametre invalide"
+		else	
+			rem $arg2
+		fi
 	;;
 	--trash)
 		trash
 	;;
 	--restore)
-		restore $arg2
+	if [[ -z $arg2 ]];then
+			echo -e "Parametre invalide"
+		else	
+			restore $arg2
+		fi
 	;;
 	*)
 		echo -e "Parametre invalide"
