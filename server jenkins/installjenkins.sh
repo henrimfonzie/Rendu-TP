a92269010fadefbaed5b00df06446d499c040db4
henri mfonzie <henri.gbetkom@yahoo.fr>
2021-04-29 10:36:27 +0200

    modification du code configuration jenkins et installation

--- server jenkins/installjenkins.sh
+++ server jenkins/installjenkins.sh
@@ -21,19 +21,24 @@ sudo apt install openjdk-11-jdk
 
 # Installer la version stable de Jenkins et ses prérequis en suivant la documentation officielle : https://www.jenkins.io/doc/book/installing/linux
 wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key | sudo apt-key add -
-sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > \
-    /etc/apt/sources.list.d/jenkins.list'
+sudo sh -c 'echo deb https://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
 
-sudo apt -y update
+sudo apt-get -y update
 
-# Démarrer le service Jenkins
+sudo apt-get -y install jenkins
 
-sudo service start jenkins
+# Démarrage de Jenkins
+
+sudo systemctl start jenkins
+
+# Démarrage de Jenkins au démarrage de la machine
+
+sudo systemctl enable jenkins
 
 # Vérifier l'état du service Jenkins
 
 sudo systemctl status jenkins
- 
+
 # Verification du démarrage en local
 
 wget localhost
@@ -53,7 +58,15 @@ sudo cat /etc/sudoers |
 
 # Afficher à la fin de l'execution du script le contenu du fichier /var/jenkins_home/secrets/initialAdminPassword pour permettre de récupérer le mot de passe
 
-cat /var/jenkins_home/secrets/initialAdminPassword
+sudo cat /var/jenkins_home/secrets/initialAdminPassword
+
+# que le port utilisé pour la connexion ssh
+
+sudo apt -y install ufw
+
+#Installer un pare-feu (genre UFW)
+
+echo y | sudo ufw enable
 
 # Install iptables
 
@@ -63,3 +76,23 @@ sudo apt install -y iptables
 
 sudo iptables -A INPUT -p tcp --dport 22 -j ACCEPT
 sudo iptables -A INPUT -p tcp --dport 80 -j ACCEPT
+
+# ouvrir le SSH
+
+sudo ufw allow ssh
+
+# Connexion ssh
+
+sudo ufw allow ssh 
+
+# Refuser le trafic entrant suivant les règles par défaut
+
+sudo ufw default deny incoming
+
+# status
+
+sudo ufw status
+
+# Afficher /var/jenkins_home/secrets/initialAdminPassword pour récuperer le mot de passe
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword 
